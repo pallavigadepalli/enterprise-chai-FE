fd9e252d6b2c78caa2bb7195a6fa3d6e57ef41bf
Silvia rodriguez <silvideastora18@gmail.com>
2024-01-24 20:53:58 -0600

    [CHA-06] create Tags component

---
+++ src/components/Tags.tsx
@@ -0,0 +1,13 @@
+
+export default function Tags() {
+  return (
+    <div className="h-16 bg-white px-4 flex justify-start align-center items-center  mb-4 container mx-auto   space-x-16">
+      <span className="text-gray-500">
+      Free trial 
+      </span>
+      <span className="text-gray-500">
+        27 min
+      </span>
+    </div>
+  )
+}
