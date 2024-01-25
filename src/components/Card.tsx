184347a742c3d5a5272ec8431f2d249b8a3f5f0e
Silvia rodriguez <silvideastora18@gmail.com>
2024-01-25 02:25:11 -0600

    [CHA-08] Create Card, Avatar & chatbox component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,12 @@
-export default function Layout() {
+import Avatar from "./Avatar";
+import photo from "../../public/Frame 10.png"
+
+export default function Card() {
   return (
-    <div className="w-4/12  bg-purple-600 bg-opacity-10 rounded-lg overflow-hidden shadow-lg">
-      <div className="bg-purple-300 p-4">
-        <p className="text-lg font-semibold text-gray-800">Nombre del Chat</p>
+    <div className="w-4/12  bg-slate-500 bg-opacity-10 rounded-lg overflow-hidden shadow-lg">
+      <div className="bg-slate-400  flex">
+        <Avatar src={photo} size={64} alt="people photo" />
+        <p className="text-lg font-medium text-gray-800 p-4 self-center">Ms. Wilson</p>
       </div>
 
       <div className="p-4">
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,18 @@
+import ChatBox from "./ChatBox";
+
 export default function Layout() {
   return (
-    <div className="w-7/12  bg-purple-600 bg-opacity-10 rounded-lg overflow-hidden shadow-lg">
+    <div className="w-7/12 h-[700px] bg-purple-600 bg-opacity-10 rounded-lg overflow-hidden shadow-lg">
       <div className="bg-purple-300 p-4">
         <p className="text-lg font-semibold text-gray-800">Nombre del Chat</p>
       </div>
   
       <div className="p-4">
         <div className="mb-4">
-          <p className="text-gray-700"><strong>Tú:</strong> Hola, ¿cómo estás?</p>
+          <ChatBox />
         </div>
         <div className="mb-4">
-          <p className="text-gray-700"><strong>Amigo:</strong> ¡Hola! Estoy bien, ¿y tú?</p>
+          <ChatBox />
         </div>
       </div>
     </div>
