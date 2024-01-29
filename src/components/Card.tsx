16bcb6285e2fafa59334340002860958c4b07abe
Silvia rodriguez <silvideastora18@gmail.com>
2024-01-29 00:54:00 -0600

    [CHA-09] Create speaker component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,21 +1,26 @@
 import Avatar from "./Avatar";
 import photo from "../../public/Frame 10.png"
+import ChatBox from "./ChatBox";
+import Speaker from "./Speaker";
 
 export default function Card() {
   return (
-    <div className="w-4/12  bg-slate-500 bg-opacity-10 rounded-lg overflow-hidden shadow-lg">
+    <div className=" bg-slate-500 bg-opacity-10 rounded-lg overflow-hidden mb-8">
       <div className="bg-slate-400  flex">
         <Avatar src={photo} size={64} alt="people photo" />
         <p className="text-lg font-medium text-gray-800 p-4 self-center">Ms. Wilson</p>
       </div>
-
+      <div className="pt-4">
+        <div className="flex justify-end">
+          <Speaker />
+        </div>
+        </div>
       <div className="p-4">
         <div className="mb-4">
-          <p className="text-gray-700"><strong>Tú:</strong> Hola, ¿cómo estás?</p>
+          <ChatBox />
         </div>
-      
         <div className="mb-4">
-          <p className="text-gray-700"><strong>Amigo:</strong> ¡Hola! Estoy bien, ¿y tú?</p>
+          <ChatBox />
         </div>
       </div>
     </div>
--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,16 +1,14 @@
 export default function ChatBox() {
   return (
-    <div className="w-full  my-4 p-4 bg-white rounded-lg shadow-md">
-  <div className="flex items-center">
-    <div className="flex-shrink-0">
-      
-    </div>
-    <div className="ml-3">
-      <div className="text-sm text-gray-900 font-extralight">13:56</div>
-      <p className="text-gray-700">I am feeling great. Thank you!</p>
+  <div className="w-full p-4 bg-white rounded-2xl shadow-md">
+    <div className="flex items-center">
+      <div className="flex-shrink-0">
+      </div>
+      <div className="ml-3">
+        <div className="text-sm text-gray-900 font-extralight">13:56</div>
+        <p className="text-gray-700">I am feeling great. Thank you!</p>
+      </div>
     </div>
   </div>
-</div>
-
   )
 }
