316771873749b3987940b7c4f8502009d9d9ba73
King Lee <c-king.lee@hulu.com>
2024-01-30 18:45:43 -0600

    styles: padding removed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Navbar() {
     return (
         <nav className="bg-white py-4 border-b border-primary">
-            <div className="container mx-auto flex items-center justify-between">
+            <div className="mx-auto flex items-center justify-between">
                 <a className="flex brand-name text-xl place-items-center" href={'/'}>
                     <span>Enterprise</span>
                     <Image src="/cup_logo.svg" alt="Logo" width={50} height={50} className={'mx-1 mb-3'}/>
