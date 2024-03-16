2c45f66d02179ec7667d467c2d7bb60ce5ed943d
King Lee <c-king.lee@hulu.com>
2024-03-15 20:58:11 -0600

    session files relocated

--- src/app/session/[id]/finished/page.tsx
+++ src/app/session/[id]/finished/page.tsx
@@ -3,7 +3,7 @@ import SessionWarning from "@/app/complete/page";
 export default function Finished() {
     return (
         <main className="w-full px-9">
-        <SessionWarning />
+            <SessionWarning />
         </main>
     )
 }
