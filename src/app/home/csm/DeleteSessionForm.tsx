9742f069b8e6afbe1fcba743894f386fe7b3045c
King Lee <c-king.lee@hulu.com>
2024-04-01 17:40:07 -0600

    sessions filtered

---
+++ src/app/home/csm/DeleteSessionForm.tsx
@@ -0,0 +1,32 @@
+'use client'
+import React from "react";
+import Image from "next/image";
+import {deleteSession} from "@/services/sessions";
+import {useFormState} from "react-dom";
+
+const initialState = {
+    message: "",
+};
+export const DeleteForm = ({id }) => {
+    const [state, formAction] = useFormState(deleteSession, initialState);
+
+    return <form action={deleteSession}
+        onSubmit={
+            async (e) => {
+                e.stopPropagation();
+
+            }
+        }
+    >
+        <input type="hidden" name={'id'} value={id}/>
+        <button type={"submit"}>
+            <Image
+                src={'/Cross Circle.png'}
+                alt='cross circle'
+                className='cursor-pointer'
+                width={24}
+                height={24}
+            />
+        </button>
+    </form>
+}
