f2bc1bede4213bccf341c81ace0d9592621690ee
King Lee <c-king.lee@hulu.com>
2024-04-01 20:48:07 -0600

    aside colors

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,22 +1,23 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Aside from "@/components/Aside";
+import React from "react";
 
 export const metadata: Metadata = {
     title: 'Enterprise Chai',
     description: 'Enterprise Chai',
 }
-
-export default function RootLayout({children}: {
+export default function RootLayout({
+    children
+}: {
     children: React.ReactNode
 }) {
     return (
         <html lang="en">
             <body>
-            <div className="flex h-screen">
-                <Aside />
-                {children}
-            </div>
+                <div className="flex h-screen">
+                    <Aside />
+                    {children}
+                </div>
             </body>
         </html>
     )
--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,6 +1,16 @@
+'use client';
 import Image from "next/image";
+import { usePathname } from 'next/navigation';
+
 
 export default function Aside() {
+    //get the current route
+    const currentRoute = usePathname()
+    const routes = [
+        {route: '/home/dashboard', icon: 'Dashboard.svg', name: 'Dashboard'},
+        {route: '/home/materials', icon: 'Materials.svg', name: 'Materials'},
+        {route: '/home/csm', icon: 'Launch.svg', name: 'CSM Companion'}
+    ]
     return <aside className="w-48 bg-customGradient p-1">
         <a className="flex mt-4 p-x-1.5 justify-center" href={'/home/dashboard'}>
             <span className={'text-primarySmall text-base font-medium'}>Enterprise</span>
@@ -10,18 +20,17 @@ export default function Aside() {
         </a>
 
         <div className="flex flex-col mt-16">
-            <a className="btn-dashboard" href={'/home/dashboard'}>
-                <Image src={'/Dashboard.svg'} alt='dashboard logo' width={18} height={18}/>
-                Dashboard
-            </a>
-            <a className="btn-dashboard" href={'/home/materials'}>
-                <Image src={'/Materials.svg'} alt='materials logo' width={18} height={18}/>
-                Materials
-            </a>
-            <a className="btn-dashboard" href={'/home/csm'}>
-                <Image src={'/Launch.svg'} alt='launch logo' width={18} height={18}/>
-                CSM Companion
-            </a>
+            {routes.map((route, index) => (
+                <a
+                    className={`btn-dashboard ${
+                        currentRoute === route.route ? ' text-primarySmall' : ''
+                    }`}
+                    href={route.route}
+                    key={index}>
+                    <Image src={`/${route.icon}`} alt={route.name} width={18} height={18}/>
+                    {route.name}
+                </a>
+            ))}
         </div>
 
     </aside>
