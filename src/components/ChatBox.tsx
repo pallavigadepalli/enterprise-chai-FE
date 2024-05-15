318eeee2c2e1be07e1c1ed5c5a85961512a0932b
King Lee <c-king.lee@hulu.com>
2024-05-15 09:09:51 -0600

    list styles fixed

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,16 +1,17 @@
 interface ChatBoxProps {
-    message: string;
+    message: any;
 }
 export default function ChatBox({message}: ChatBoxProps) {
-    const currentTime = new Date().toLocaleTimeString();
     return (
         <div className="w-full p-4 bg-white rounded-2xl shadow-md">
             <div className="flex items-center">
                 <div className="flex-shrink-0">
                 </div>
                 <div className="ml-3">
-                    <div className="text-sm text-grayCustom font-extralight">{currentTime}</div>
-                    <p>{message}</p>
+                    <div className="text-sm text-grayCustom font-extralight">{message.timestamp}</div>
+                    <div
+                        dangerouslySetInnerHTML={{__html: message.text}}
+                    ></div>
                 </div>
             </div>
         </div>
