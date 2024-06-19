efe628d49d5084fd2424cfc36a33caf2680c9769
King Lee <c-king.lee@hulu.com>
2024-06-19 11:59:32 -0600

    Added need libraries to include different formats in llm output

--- src/components/ChatBox.tsx
+++ src/components/ChatBox.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import {useMarkdownProcessor} from "@/hooks/markdown";
 
 interface ChatBoxProps {
     message: {
@@ -8,6 +9,7 @@ interface ChatBoxProps {
     }
 }
 export default function ChatBox({message}: ChatBoxProps) {
+    const content = useMarkdownProcessor(message.answer);
     return (
         <div className={""}>
             <div className="w-full p-4 bg-white rounded-2xl shadow-md mb-3">
@@ -16,9 +18,11 @@ export default function ChatBox({message}: ChatBoxProps) {
                     </div>
                     <div className="ml-3">
                         <div className="text-sm text-grayCustom font-extralight text-xs">{message.timestamp}</div>
-                        <div
-                            dangerouslySetInnerHTML={{__html: message.answer}}
-                        ></div>
+                        <li className="flex flex-col flex-1 min-w-0 gap-1 ml-6 selection:bg-emerald-300 selection:text-emerald-900">
+                            <div className="p-2 lg:p-6 border-2 border-emerald-200 rounded-lg bg-emerald-50 text-emerald-900 min-w-0 [&>*:first-child]:mt-0 [&>*:last-child]:mb-0">
+                                {content}
+                            </div>
+                        </li>
                     </div>
                 </div>
             </div>
