0c13fb27c1df332567fcde26e67824d9481aa5e1
King Lee <c-king.lee@hulu.com>
2024-06-23 09:54:39 -0600

    capture controller updated

--- src/app/home/csm/CSMForm.tsx
+++ src/app/home/csm/CSMForm.tsx
@@ -23,7 +23,7 @@ export const CSMForm = ({phases, products}: CSMFormProps) => {
     const [state, formAction] = useFormState(saveSession, initialState);
     const [customerName, setCustomerName] = React.useState<string | string[]>('');
 
-    if (state.session) {
+    if (state?.session) {
         window.open(`/session/${state.session}/active`, "CSMWindow", "popup")
     }
     return <form className={'flex flex-col gap-4'} action={formAction}>
